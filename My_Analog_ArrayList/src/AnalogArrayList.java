61b31da4bda9ea2044d444eb3f63c2dbb3d6d0ed
Roman Manzhula <roman1985al@ukr.net>
2024-12-22 20:50:24 +0200

    Update AnalogArrayList.java
    In Ukrainian

--- My_Analog_ArrayList/src/AnalogArrayList.java
+++ My_Analog_ArrayList/src/AnalogArrayList.java
@@ -2,80 +2,78 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class AnalogArrayList<T> {
-    private final int DEFAULT_CAPACITY = 16; //начальный размер массива
-    private final int REDUCTION_SIZE = 4; //размер массива для уменьшения
-    private Object[] arrayObjects = new Object[DEFAULT_CAPACITY]; //массив со стартовым размером
-    private int pointer = 0; //указатель на предельную границу массива
+    private final int DEFAULT_CAPACITY = 16; // початковий розмір масиву
+    private final int REDUCTION_SIZE = 4; // розмір масиву для зменшення
+    private Object[] arrayObjects = new Object[DEFAULT_CAPACITY]; // масив із початковим розміром
+    private int pointer = 0; // вказівник на граничну межу масиву
 
-    public boolean add(T elementOfArray) {//добавление элемента в массив
-        if (pointer == arrayObjects.length - 1) { //если достигли предела границы массива, то
-            Object[] newSizeArrayObjects = new Object[arrayObjects.length * 2]; //увеличиваем размер массива в два раза
-            System.arraycopy(arrayObjects, 0, newSizeArrayObjects, 0, pointer);//осуществляем копирование
-            //заполненного массива в новый увеличинный массив
-            arrayObjects = newSizeArrayObjects; //подменяем ссылки на массивы
+    public boolean add(T elementOfArray) { // додавання елемента до масиву
+        if (pointer == arrayObjects.length - 1) { // якщо досягли межі масиву, то
+            Object[] newSizeArrayObjects = new Object[arrayObjects.length * 2]; // збільшуємо розмір масиву вдвічі
+            System.arraycopy(arrayObjects, 0, newSizeArrayObjects, 0, pointer); // копіюємо вміст 
+            // заповненого масиву в новий збільшений масив
+            arrayObjects = newSizeArrayObjects; // змінюємо посилання на новий масив
         }
-        arrayObjects[pointer] = elementOfArray;//крайнему элементу присваиваем значение добавленного элемента
-        pointer++;//инкрементируем указатель предельной границы массива
+        arrayObjects[pointer] = elementOfArray; // останньому елементу присвоюємо значення доданого елемента
+        pointer++; // інкрементуємо вказівник межі масиву
         return true;
     }
 
-    public boolean remove(int index) { //удаление элемента массива по индексу
-        for (int i = index; i < pointer; i++) { //циклом от указанного индекса до указателя предельной границы массива
-            arrayObjects[i] = arrayObjects[i + 1];//все элементы справа от удаляемого перемещаем в левую сторону
+    public boolean remove(int index) { // видалення елемента масиву за індексом
+        for (int i = index; i < pointer; i++) { // циклом від зазначеного індексу до межі масиву
+            arrayObjects[i] = arrayObjects[i + 1]; // всі елементи праворуч від видаленого зсуваємо вліво
         }
-        arrayObjects[pointer] = null;//последнему элементу присваиваем значение null
-        pointer--;//декрементируем указатель предельной границы массива
+        arrayObjects[pointer] = null; // останньому елементу присвоюємо значення null
+        pointer--; // декрементуємо вказівник межі масиву
 
-        if (arrayObjects.length > DEFAULT_CAPACITY && pointer < arrayObjects.length / REDUCTION_SIZE) { //если элементов
-            //в масиве меньше, чем его длина в REDUCTION_SIZE раз, то уменьшаем массив в 2 раза (для экономии памяти). Но
-            //уменьшее массива до номинального размера.
-            Object[] newSizeArrayObjects = new Object[arrayObjects.length / 2]; //уменьшаем размер массива в два раза
-            System.arraycopy(arrayObjects, 0, newSizeArrayObjects, 0, pointer);//осуществляем копирование
-            //заполненного массива в новый уменьшенный массив
-            arrayObjects = newSizeArrayObjects; //подменяем ссылки на массивы
+        if (arrayObjects.length > DEFAULT_CAPACITY && pointer < arrayObjects.length / REDUCTION_SIZE) { // якщо елементів 
+            // у масиві менше, ніж його довжина, поділена на REDUCTION_SIZE, то зменшуємо масив удвічі (для економії пам’яті). Але 
+            // не зменшуємо масив до номінального розміру.
+            Object[] newSizeArrayObjects = new Object[arrayObjects.length / 2]; // зменшуємо розмір масиву удвічі
+            System.arraycopy(arrayObjects, 0, newSizeArrayObjects, 0, pointer); // копіюємо вміст 
+            // заповненого масиву в новий зменшений масив
+            arrayObjects = newSizeArrayObjects; // змінюємо посилання на новий масив
         }
         return true;
     }
 
-    public int size() {//метод возврата размера массива
-        return pointer;//размер равен предельному указателю предедльной границы массива
+    public int size() { // метод повернення розміру масиву
+        return pointer; // розмір дорівнює граничному вказівнику масиву
     }
-    public T get(int index) { //возвращаем элемент из массива по индексу
-        T obj = null;//обьявляем ссылку типа класса Т
-        if (index > pointer || index < 0) {//если указанный индекс элемента будет больше предельной границы массива или иметь
-            //отрицательное значение, то
-            obj = (T) new String("IndexOutOfBoundsException");//ссылке obj присваиваем значение = строковому литералу
-        } else {//иначе
-            obj = (T) arrayObjects[index];//ссылке obj присваиваем значение = обьекту массива под данным индексом
+    public T get(int index) { // повернення елемента з масиву за індексом
+        T obj = null; // оголошуємо посилання типу T
+        if (index > pointer || index < 0) { // якщо зазначений індекс елемента більший за межу масиву або від’ємний, то
+            obj = (T) new String("IndexOutOfBoundsException"); // посиланню obj присвоюємо значення строкового літералу
+        } else { // інакше
+            obj = (T) arrayObjects[index]; // посиланню obj присвоюємо значення елемента масиву за індексом
         }
-        return (T) obj;//возвращаем содержимое ссыл.переменной (приведение типа к Generics для элемента массива по индексу)
-
+        return (T) obj; // повертаємо вміст посилання (з приведенням типу Generics для елемента масиву за індексом)
     }
 
-    public void print() {//метод для вывода на консоль массива
-        Iterator<Object> iterator = Arrays.stream(arrayObjects).iterator();//получаем итератор для массива
-        while (iterator.hasNext()) {//проверяем есть ли еще элементы в массиве
-            var object = iterator.next();//получаем текущий элемент и переходим на следующий
-            if (object != null) System.out.println(object);//проверяем чтобы элемент не был null - печатаем данный элемент
+    public void print() { // метод для виведення масиву на консоль
+        Iterator<Object> iterator = Arrays.stream(arrayObjects).iterator(); // отримуємо ітератор для масиву
+        while (iterator.hasNext()) { // перевіряємо, чи є ще елементи в масиві
+            var object = iterator.next(); // отримуємо поточний елемент та переходимо до наступного
+            if (object != null) System.out.println(object); // перевіряємо, щоб елемент не був null - виводимо цей елемент
         }
     }
 
-    public void print2() {//второй метод для вывода на консоль содержимого массива
-        for (int i = 0; i < arrayObjects.length; i++) {//циклом проходим по массиву arrayObjects
-            if (arrayObjects[i] != null) {//если элемент массива не равен null, то
-                System.out.println(arrayObjects[i]);//печатаем данный элемент
+    public void print2() { // другий метод для виведення вмісту масиву на консоль
+        for (int i = 0; i < arrayObjects.length; i++) { // циклом проходимо по масиву arrayObjects
+            if (arrayObjects[i] != null) { // якщо елемент масиву не дорівнює null, то
+                System.out.println(arrayObjects[i]); // виводимо цей елемент
             }
         }
-        System.out.println();//разделитель - новая строка
+        System.out.println(); // роздільник - новий рядок
     }
 
-    public boolean contains(T elementOfArray) {//метод для сравнения обьектов в массиве
-        int indexOf = 0;//создаем переменную (псевдо-метод indexOf) и инициализируем ее 0
-        for (Object element : arrayObjects) {//циклом for-each (для каждого єлемента из массива) проходим по массиву
-            if (element.equals(elementOfArray)) {//если єлемент массива будет равен сравниваемому єлементу, то
-                indexOf++;//инкрементируем переменную (псевдо-метод), главное чтоб она не имела отрицательное значение(т.е. true)
-            } else {//иначе
-                indexOf = -1;//присваиваем переменной (псевдо-методу) значение -1 (т.е. false)
+    public boolean contains(T elementOfArray) { // метод для порівняння об'єктів у масиві
+        int indexOf = 0; // створюємо змінну (псевдометод indexOf) та ініціалізуємо її значенням 0
+        for (Object element : arrayObjects) { // циклом for-each (для кожного елемента з масиву) проходимо по масиву
+            if (element.equals(elementOfArray)) { // якщо елемент масиву дорівнює порівнюваному елементу, то
+                indexOf++; // інкрементуємо змінну (псевдометод), головне, щоб вона не мала від’ємного значення (тобто true)
+            } else { // інакше
+                indexOf = -1; // присвоюємо змінній (псевдометоду) значення -1 (тобто false)
             }
         }
         return indexOf >= 0;
